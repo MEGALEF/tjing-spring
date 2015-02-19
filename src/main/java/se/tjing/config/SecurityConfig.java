54b1a939080b0f23da894bf451eba5c187a0b219
MEGALEF <nossredna.sennahoj@gmail.com>
2015-02-19 12:59:26 +0100

    Attempt to include password encryption

--- src/main/java/se/tjing/config/SecurityConfig.java
+++ src/main/java/se/tjing/config/SecurityConfig.java
@@ -12,9 +12,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.encrypt.Encryptors;
 import org.springframework.security.crypto.encrypt.TextEncryptor;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
@@ -65,7 +65,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
+		PasswordEncoder encoder = new BCryptPasswordEncoder();
+		return encoder;
+		// return NoOpPasswordEncoder.getInstance();
 	}
 
 	@Bean
