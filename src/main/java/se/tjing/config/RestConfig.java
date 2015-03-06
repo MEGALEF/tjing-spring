2ab3b211b2633ab7dfe8ecacee31ee3b7f13dfdd
MEGALEF <nossredna.sennahoj@gmail.com>
2015-03-06 15:50:29 +0100

    Fixed resource problem

--- src/main/java/se/tjing/config/RestConfig.java
+++ src/main/java/se/tjing/config/RestConfig.java
@@ -6,14 +6,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperFactoryBean;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.joda.JodaModule;
 
 @Configuration
-@EnableWebMvc
 public class RestConfig extends WebMvcConfigurerAdapter {
 
 	private ObjectMapper objectMapper() {
