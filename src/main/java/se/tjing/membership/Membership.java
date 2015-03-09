00d56b3cb63bf68d1f0934b42f677c520f625a9b
MEGALEF <nossredna.sennahoj@gmail.com>
2015-03-09 16:02:34 +0100

    Cleanup

--- src/main/java/se/tjing/membership/Membership.java
+++ src/main/java/se/tjing/membership/Membership.java
@@ -10,8 +10,6 @@ import se.tjing.common.BaseEntity;
 import se.tjing.pool.Pool;
 import se.tjing.user.Person;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-
 @Entity
 public class Membership extends BaseEntity {
 
@@ -20,10 +18,9 @@ public class Membership extends BaseEntity {
 	Integer id;
 
 	@ManyToOne
-	@JsonBackReference
 	private Person member;
+
 	@ManyToOne
-	@JsonBackReference
 	private Pool pool;
 
 	public Person getMember() {
