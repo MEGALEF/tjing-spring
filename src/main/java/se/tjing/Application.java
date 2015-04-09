4c0e991a7f9d4b6da0902b61b0fc2d1eec5d1634
MEGALEF <nossredna.sennahoj@gmail.com>
2015-04-09 08:19:15 +0200

    Revert "Https enabled"
    This reverts commit a2c0e9bf966b4cb207cbe46061716e7787dd23c3.

--- src/main/java/se/tjing/Application.java
+++ src/main/java/se/tjing/Application.java
@@ -25,7 +25,7 @@ public class Application {
 	public static void main(String[] args) {
 		String webPort = System.getenv("PORT");
 		if (webPort == null || webPort.isEmpty()) {
-			webPort = "8443";
+			webPort = "8080";
 		}
 		System.setProperty("server.port", webPort);
 
